d2ee57ec0b1b0900d1d47fb162655e54eca49710
Ethan Lin <eyl45@cornell.edu>
2023-12-08 23:42:12 -0500

    Completed redesign of active state textures

--- game/main.ml
+++ game/main.ml
@@ -1,28 +1,6 @@
 open Lib
 open Raylib
 
-(* state temp button hitbox *)
-let state_hb = Rectangle.create 300. 350. 150. 100.
-
-(* phase temp button hitbox *)
-let phase_hb = Rectangle.create 750. 350. 150. 100.
-
-let highlight_button_state mouse hitbox highlight (x, y) =
-  if check_collision_point_rec mouse hitbox then
-    match is_mouse_button_pressed MouseButton.Left with
-    | false -> draw_texture highlight x y Color.raywhite
-    | true ->
-        Constants.game_active := None;
-        Constants.game_state := END
-
-let highlight_button_phase mouse hitbox highlight (x, y) =
-  if check_collision_point_rec mouse hitbox then
-    match is_mouse_button_pressed MouseButton.Left with
-    | false -> draw_texture highlight x y Color.raywhite
-    | true ->
-        Constants.game_active :=
-          Some (Game.change_phase (Constants.get_game ()))
-
 (* Game setup function *)
 let setup () =
   init_window Constants.screen_width Constants.screen_height "risk_of_ocaml";
@@ -55,14 +33,8 @@ let rec loop () =
       | ACTIVE ->
           begin_drawing ();
           clear_background Color.raywhite;
-          Active.draw_active ();
-          let state, phase = Active.get_hl () in
           let mouse = get_mouse_position () in
-          (* change state *)
-          highlight_button_state mouse state_hb state (300, 350);
-          (* change phase *)
-          highlight_button_phase mouse phase_hb phase (750, 350);
-
+          Active.draw_active mouse;
           end_drawing ();
           loop ()
       | END ->
