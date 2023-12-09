71cd4a753776bdeab08c2479de66b422f91a62cc
jmyphn <jmy.phn2@gmail.com>
2023-12-08 22:45:26 -0500

    Restructure Active state - Move functions from game/main.ml to lib/active.ml

--- lib/start.ml
+++ lib/start.ml
@@ -1,5 +1,4 @@
 open Raylib
-open Raygui
 
 type t = {
   bg : Texture2D.t;
@@ -9,8 +8,6 @@ type t = {
 
 let offset = 10
 let start = ref None
-let tb_edit = ref false
-let tb_text = ref "hello"
 
 let start_hb =
   Rectangle.create
@@ -32,11 +29,6 @@ let initialize_start () =
         button_hl = start_button_highlight_texture;
       }
 
-let grab_text_in_box () =
-  match is_key_pressed Enter with
-  | true -> print_endline !tb_text
-  | _ -> ()
-
 let get_button_hl () =
   match !start with
   | None -> failwith "Failed to load in start textures"
@@ -45,7 +37,7 @@ let get_button_hl () =
 let highlight_button_start mouse =
   let start_hl = (Option.get !start).button_hl in
   if check_collision_point_rec mouse start_hb then
-    match is_mouse_button_down MouseButton.Left with
+    match is_mouse_button_pressed MouseButton.Left with
     | false ->
         draw_texture start_hl (681 - offset) (570 - offset)
           Constants.default_color
@@ -60,16 +52,4 @@ let draw_start mouse =
   let origin = Vector2.create 0. 0. in
   draw_texture_pro start.bg source dest origin 0. Constants.default_color;
   highlight_button_start mouse;
-  draw_texture start.button 681 570 Constants.default_color;
-  grab_text_in_box ();
-  (* rect: shape and position of the text box on screen *)
-  let rect = Rectangle.create 25.0 215.0 125.0 30.0 in
-  match text_box rect !tb_text !tb_edit with
-  (* vl is the text inside the textbox *)
-  | vl, true ->
-      tb_edit := not !tb_edit;
-      tb_text := vl
-  | vl, false ->
-      tb_text := vl;
-
-      set_style (TextBox `Text_alignment) TextAlignment.(to_int Left)
+  draw_texture start.button 681 570 Constants.default_color
