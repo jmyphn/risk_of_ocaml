1bc480090374fb13afeadbe8416631321501cb45
jmyphn <jmy.phn2@gmail.com>
2023-12-10 13:22:20 -0500

    Remove view.mli

--- lib/view.mli
+++
@@ -1,7 +0,0 @@
-(** Handles input values from the GUI. *)
-
-val value_from_gui : string option ref
-(** [value_from_gui] is the value from the GUI *)
-
-val get_value_from_gui : unit -> string option
-(** [get_value_from_gui u] gets the value input from the GUI *)
